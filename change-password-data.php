052f62e750802e67cbbc2fbc6291b5f337e38be6
codebybablu <bablu.mailaxy@gmail.com>
2024-11-24 21:48:51 +0530

    comment added

--- change-password-data.php
+++ change-password-data.php
@@ -10,18 +10,22 @@ $confirm_password = $_POST['confirm_password'];
 $query = "SELECT * FROM userinfo WHERE email = '".$_SESSION['email']."'";
 // query
 $result = mysqli_query($conn, $query);
+
 $user = mysqli_fetch_assoc($result);
+
 // for password verification
 if (password_verify($current_password, $user['password'])) {
   echo "Current password is incorrect.";
   exit;
 }
 
+// new passord not confirm
 if ($new_password != $confirm_password) {
   echo "New password and confirm password do not match.";
   exit;
 }
 
+// new password hash
 $new_password_hash = password_hash($new_password, PASSWORD_DEFAULT);
 
 $query = "UPDATE userinfo SET password = '".$new_password_hash."' WHERE email = '".$_SESSION['email']."'";
